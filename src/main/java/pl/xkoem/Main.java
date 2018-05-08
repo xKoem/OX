c95893b08cf26b707a1019eb672601dba9b6e9c1
koem <kosmi.bartek@gmail.com>
2018-05-08 13:18:58 +0200

    dodano drugi jezyk oraz poprawiona literowke

--- src/main/java/pl/xkoem/Main.java
+++ src/main/java/pl/xkoem/Main.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        UserInterface userInterface = new UserInterface(new Scanner(System.in)::nextLine, System.out::println, FileReader.readLanguageFile("languages/PL.lang"));
+        UserInterface userInterface = new UserInterface(new Scanner(System.in)::nextLine, System.out::println, FileReader.readLanguageFile("languages/ENG.lang"));
         OXGame oxGame = new OXGame(new Players(), userInterface, new GameConfiguration());
         oxGame.run();
     }
